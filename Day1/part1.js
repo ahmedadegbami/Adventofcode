989f1816e1bbbdd538082b44c84ba2f23d638171
Ahmed Adegbami <ahmed.adegbami@orbitlabs.de>
2023-12-11 16:52:20 +0100

    Day 2 part 1 and prettier

--- Day1/part1.js
+++ Day1/part1.js
@@ -1,12 +1,11 @@
- import fs from "fs"
+import fs from "fs";
 
- let input = fs.readFileSync("./day1/input.txt").toString()
+let input = fs.readFileSync("./day1/input.txt").toString();
 
+let total = 0;
+for (let line of input.split("\n")) {
+  const numbers = line.split("").filter((item) => !isNaN(parseInt(item)));
+  total += parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`);
+}
 
- let total = 0
- for (let line of input.split("\n")) {
-    const numbers = line.split("").filter(item => !isNaN(parseInt(item)))
-   total += parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`)
- }
-
- console.log(total)
+console.log(total);
--- Day2/index.js
+++ Day2/index.js
@@ -1,6 +1,38 @@
-import fs from "fs"
+import fs from "fs";
 
-const input = fs.readFileSync("./day2/input.txt").toString()
+const input = fs.readFileSync("./day2/input.txt").toString();
 
-console.log({input})
+let total = 0;
 
+for (let line of input.split("\n")) {
+  let valid = true;
+  const [gameId, values] = line.split(": ");
+  const subsets = values.split("; ");
+
+  for (let set of subsets) {
+    const cubeBalls = {
+      red: 12,
+      green: 13,
+      blue: 14,
+    };
+
+    let cubes = set.split(", ");
+    for (const cube of cubes) {
+      let [number, color] = cube.split(" ");
+      cubeBalls[color] -= parseInt(number);
+
+      for (let key of Object.keys(cubeBalls)) {
+        if (cubeBalls[key] < 0) {
+          valid = false;
+          break;
+        }
+      }
+    }
+  }
+
+  if (valid) {
+    total += parseInt(gameId.split(" ")[1]);
+  }
+}
+
+console.log(total);
